8a80ebb04595d0603800f86792832f12b9b4b113
Andrew Cutler <AndrewKCutler@gmail.com>
2023-12-30 21:46:56 -0500

    added listener

--- client/index.js
+++ client/index.js
@@ -1,3 +1,8 @@
-document.onload = function () {
+window.onload = function () {
 	console.log('init');
+
+	const input = document.querySelector('#upload');
+	input.addEventListener('change', function () {
+		console.log(input.files);
+	});
 };
