9f90cb93f1a48a0c3c1c14c8d28ace7902132d3b
Andrew Cutler <AndrewKCutler@gmail.com>
2023-12-30 21:51:48 -0500

    set up post route to handle saving files

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"log"
 	"net/http"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("./client"))
+	http.HandleFunc("/save", saveFile)
 	http.Handle("/", fs)
 
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		panic(err)
 	}
+}
+
+func saveFile(w http.ResponseWriter, h *http.Request) {
+	body, err := io.ReadAll(h.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(body)
 }
