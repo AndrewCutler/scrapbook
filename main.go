4d40c36346b6fd2fc888ad121dccc4ec35d823ea
Andrew Cutler <AndrewKCutler@gmail.com>
2023-12-31 11:52:17 -0500

    save uploaded files to local disk

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
+
+var fileDir = "./files"
 
 func main() {
 	fs := http.FileServer(http.Dir("./client"))
@@ -18,9 +21,44 @@ func main() {
 }
 
 func saveFile(w http.ResponseWriter, h *http.Request) {
-	body, err := io.ReadAll(h.Body)
-	if err != nil {
-		log.Fatal(err)
+	h.ParseMultipartForm(200)
+
+	form := h.MultipartForm
+
+	for key := range form.File {
+		file, header, err := h.FormFile(key)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Println(header.Filename)
+
+		out, err := os.Create(fileDir + header.Filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+
+		if _, err := io.Copy(out, file); err != nil {
+			log.Fatal(err)
+		}
 	}
-	go fmt.Println(body)
+	// form, err := h.MultipartReader()
+	// for {
+	// 	part, err_part := form.NextPart()
+	// 	if err_part == io.EOF {
+	// 		break
+	// 	}
+	// 	if part.FormName() == "file" {
+	// 		x, err := part.Read()
+	// 		i
+	// 		// go fmt.Println()
+	// 	}
+	// }
+	// body, err := io.ReadAll(h.Body)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// go fmt.Println(body)
 }
