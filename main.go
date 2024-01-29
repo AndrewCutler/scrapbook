5b73e7e0ef96826511de591a01dc8e5665ab1015
Andrew Cutler <AndrewKCutler@gmail.com>
2024-01-28 19:14:53 -0500

    created handlers package

--- main.go
+++ main.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/subtle"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"image"
-	"io"
 	"log"
 	"net/http"
 	"os"
@@ -16,29 +10,16 @@ import (
 	"strings"
 	"time"
 
-	utils "scrapbook/utils"
+	handlers "scrapbook/handlers"
 
 	"github.com/gorilla/mux"
 )
 
 var fileDir = "./files/"
 
-type FileMeta struct {
-	Name      string
-	Size      int64
-	Thumbnail string
-	Height    int
-	Width     int
-}
-
 type SpaHandler struct {
 	staticPath string
 	indexPath  string
-}
-
-type Config struct {
-	Username string
-	Password string
 }
 
 func (h SpaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -70,11 +51,11 @@ func (h SpaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/api/test", useBasicAuth(getTestFileHandler)).Methods("GET")
-	r.HandleFunc("/api/save", saveFileHandler).Methods("POST")
-	r.HandleFunc("/api/files", buildFileListHandler).Methods("GET")
-	r.HandleFunc("/api/login", loginHandler).Methods("POST")
-	r.HandleFunc("/api/files/{filename}", useBasicAuth(getFileHandler)).Methods("GET")
+	r.HandleFunc("/api/test", useBasicAuth(handlers.GetTestFileHandler)).Methods("GET")
+	r.HandleFunc("/api/save", handlers.SaveFileHandler).Methods("POST")
+	r.HandleFunc("/api/files", handlers.BuildFileListHandler).Methods("GET")
+	r.HandleFunc("/api/login", handlers.LoginHandler).Methods("POST")
+	r.HandleFunc("/api/files/{filename}", useBasicAuth(handlers.GetFileHandler)).Methods("GET")
 
 	spa := SpaHandler{staticPath: "client", indexPath: "index.html"}
 	r.PathPrefix("/").Handler(spa)
@@ -86,149 +67,6 @@ func main() {
 	}
 
 	log.Fatal(srv.ListenAndServe())
-}
-
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	body, readErr := io.ReadAll(r.Body)
-	if readErr != nil {
-		fmt.Println(readErr)
-	}
-
-	var creds Config
-	deserializeErr := json.Unmarshal(body, &creds)
-	if deserializeErr != nil {
-		fmt.Println(deserializeErr)
-	}
-
-	hasher := sha1.New()
-	hasher.Write([]byte(creds.Username))
-	usernameSha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
-	hasher.Reset()
-	hasher.Write([]byte(creds.Password))
-	pwSha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
-
-	config := readConfig()
-
-	if usernameSha == config.Username && pwSha == config.Password {
-		fmt.Println("Authenticated.")
-	} else {
-		fmt.Println("Not authenticated.")
-	}
-}
-
-func getTestFileHandler(w http.ResponseWriter, r *http.Request) {
-	b, err := os.ReadFile("./client/test.js")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	w.Write(b)
-}
-
-func saveFileHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(200)
-
-	form := r.MultipartForm
-	files := form.File["files"]
-	if files == nil {
-		log.Fatal("No files received")
-	}
-
-	for _, f := range files {
-		file, err := f.Open()
-		if err != nil {
-			log.Fatal(err)
-		}
-		out, err := os.Create(fileDir + f.Filename)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer out.Close()
-
-		if _, err := io.Copy(out, file); err != nil {
-			log.Fatal(err)
-		}
-
-		utils.CreateThumbnail(f.Filename, fileDir)
-	}
-}
-
-func buildFileListHandler(w http.ResponseWriter, r *http.Request) {
-	dir, err := os.ReadDir(fileDir)
-	if err != nil {
-		log.Fatal("Cannot read file directory")
-	}
-
-	var filemeta []FileMeta
-	for _, f := range dir {
-		filename := fileDir + f.Name()
-		b, err := os.ReadFile("." + utils.GetThumbnailPathFromFilename(filename))
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		if !strings.HasSuffix(f.Name(), ".mp4") && !strings.HasSuffix(f.Name(), ".webm") /* || etc. */ {
-			continue
-		}
-
-		fi, err := os.Stat(filename)
-		if err != nil {
-			fmt.Println(err)
-		}
-		var thumbnail string
-		if err != nil {
-			wd, _ := os.Getwd()
-			fmt.Println(wd, "\n", err)
-			thumbnail = ""
-		} else {
-			thumbnail = "data:image/jpeg;base64," + base64.StdEncoding.EncodeToString(b)
-		}
-
-		// get image dimensions
-		height := 200
-		width := 200
-		reader, err := os.Open("." + utils.GetThumbnailPathFromFilename(filename))
-		defer reader.Close()
-		if err == nil {
-			im, _, err := image.DecodeConfig(reader)
-			height = im.Height
-			width = im.Width
-			if err != nil {
-				fmt.Println(err)
-			}
-		} else {
-			fmt.Println(err)
-		}
-
-		filemeta = append(filemeta, FileMeta{
-			Name:      fi.Name(),
-			Size:      fi.Size(),
-			Thumbnail: thumbnail,
-			Height:    height,
-			Width:     width,
-		})
-	}
-
-	w.Header().Set("Content-Type", "application-json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	json.NewEncoder(w).Encode(filemeta)
-
-}
-
-func getFileHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	name := vars["filename"]
-
-	if name == "" {
-		fmt.Println("empty name received")
-		return
-	}
-	b, err := os.ReadFile(fileDir + name)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	w.Write(b)
 }
 
 func useBasicAuth(next http.HandlerFunc) http.HandlerFunc {
@@ -251,17 +89,4 @@ func useBasicAuth(next http.HandlerFunc) http.HandlerFunc {
 
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 	})
-}
-
-func readConfig() Config {
-	file, _ := os.Open("./config.json")
-	defer file.Close()
-	decoder := json.NewDecoder(file)
-	config := Config{}
-	err := decoder.Decode(&config)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return config
 }
